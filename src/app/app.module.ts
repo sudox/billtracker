c9216085a1fb0fb087e949a174fe9937f5da193c
Melanie <mjg72@zips.uakron.edu>
2021-09-13 16:50:59 -0400

    Add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BillTableComponent } from './bill-table/bill-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BillTableComponent
   ],
   imports: [
     BrowserModule,
